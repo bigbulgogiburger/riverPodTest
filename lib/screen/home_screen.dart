b8e2b9d0047dbac0bc1e2630ac787526ccb91436
dhpyun <dhpyun@dbinc.co.kr>
2024-03-20 16:55:36 +0900

    riverpod 학습중

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:riverpodtest/layout/default_layout.dart';
 import 'package:riverpodtest/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpodtest/screen/family_modifier_screen.dart';
 import 'package:riverpodtest/screen/future_provider_screen.dart';
+import 'package:riverpodtest/screen/listen_provider_screen.dart';
+import 'package:riverpodtest/screen/select_provider_screen.dart';
 import 'package:riverpodtest/screen/state_notifier_provider_screeen.dart';
 import 'package:riverpodtest/screen/state_provider_screen.dart';
 import 'package:riverpodtest/screen/stream_provider_screen.dart';
@@ -75,6 +77,26 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ListenProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ListenProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => SelectProviderScreen(),
+                ),
+              );
+            },
+            child: Text('SelectProviderScreen'),
           ),
         ],
       ),
